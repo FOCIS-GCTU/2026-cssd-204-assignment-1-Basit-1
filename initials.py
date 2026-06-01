bd97230bb996e0331049511a106e74a1e3fbd955
Basit-1 <2425402315@live.gctu.edu.gh>
2026-06-01 07:00:41 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+#File:initials.py
+#Description:Prints out the initials of my full  name
+#Assignment Number:1
+#
+#Name:Abdul Basit Hassan
+#STUDENT ID:2425402315
+#Email:2425402315@live.gctu.edu.gh
+#Grader:Mr.Augustus Buckman
+#
+#On my honor,Abdul Bassit Hassan, this programming assignment is my own work
+#and I have not provided this code to any other student
 
 def main():
-  pass #code goes here
+    #the codes below will print my initial in large letters
+    print()
+    print('...ABH')
+    print()
 
+    print('...AAAAAAAAAAAA........BBBBBBBBBB..........HHHH....HHHH.....')
+    print('...AAAAAAAAAAAA........BBBBBBBBBBBB........HHHH....HHHH.....')
+    print('...AAA......AAA........BBBBB..BBBBB........HHHH....HHHH.....')
+    print('...AAA......AAA........BBBBBBBBBBBB........HHHH....HHHH.....')
+    print('...AAAAAAAAAAAA........BBBBBBBBBBB.........HHHHHHHHHHHH.....')
+    print('...AAAAAAAAAAAA........BBBBBBBBBB..........HHHHHHHHHHHH.....')
+    print('...AAA......AAA........BBBBBBBBBBB.........HHHH....HHHH.....')
+    print('...AAA......AAA........BBBBB..BBBBB........HHHH....HHHH.....')
+    print('...AAA......AAA...**...BBBBBBBBBBBB...**...HHHH....HHHH...**')
+    print('...AAA......AAA...**...BBBBBBBBBB.....**...HHHH....HHHH...**')
+    print()
 
-
-main()
+    main()
+    
